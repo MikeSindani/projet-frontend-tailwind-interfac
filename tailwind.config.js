52bae4ba0ea4b3c8752ebcc397d3d22a84b363b3
MikeSindani <mikems@live.fr>
2023-02-13 22:25:35 +0200

    add new page description and article page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       extend: {
         fontFamily: {
           'poppins': ['Poppins', 'sans-serif'],
-          'oswald': ['Oswald', 'sans-serif'],
+          'oswald': ['Oswald', 'Poppins','sans-serif'],
         },
         colors: {
           transparent: 'transparent',
@@ -17,6 +17,13 @@ module.exports = {
           // Complex site-specific column configuration
           'main': '2fr 1fr',
           'card': '1fr 2fr',
+        },
+        gridTemplateRows: {
+          // Simple 8 row grid
+          'card-y': '1f 2fr',
+  
+          // Complex site-specific row configuration
+          'layout': '200px minmax(900px, 1fr) 100px',
         }
       },
       
